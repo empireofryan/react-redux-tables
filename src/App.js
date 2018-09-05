122abb3cd9a1613a3c9fe83e92095c727befe974
empireofryan <rjohnson2001@gmail.com>
2018-09-05 15:42:14 -0400

    button commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React, { Component } from 'react';
-import ReactTable from "react-table";
-// import { Button } from '@rebass/grid';
+import { Button } from '@rebass/grid';
 import './App.css';
-import 'react-table/react-table.css';
 
 class App extends Component {
   render() {
-    const data = [{
-      name: 'Tanner Linsley',
-      age: 26,
-      friend: {
-        name: 'Jason Maurer',
-        age: 23,
-      }
-    }]
-  
-    const columns = [{
-      Header: 'Name',
-      accessor: 'name' // String-based value accessors!
-    }, {
-      Header: 'Age',
-      accessor: 'age',
-      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-    }, {
-      id: 'friendName', // Required because our accessor is not a string
-      Header: 'Friend Name',
-      accessor: d => d.friend.name // Custom value accessors!
-    }, {
-      Header: props => <span>Friend Age</span>, // Custom header components!
-      accessor: 'friend.age'
-    }]
-    return(
-      <ReactTable
-        data={data}
-        columns={columns}
-      />
-    )
+    return (
+      <div>
+        <Button>
+          Hi
+        </Button>
+      </div>
+    );
   }
 }
+
 export default App;
